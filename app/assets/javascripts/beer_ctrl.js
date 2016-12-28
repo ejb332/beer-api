abc25320cdf77e39aa78e951032ea7b815ff16c4
Eric Biel <eric.j.biel@gmail.com>
2016-12-27 19:36:50 -0600

    Add angular sort actions

---
+++ app/assets/javascripts/beer_ctrl.js
@@ -0,0 +1,54 @@
+/* global angular */
+
+angular.module("app").controller("beerCtrl", function($scope, $http) {
+
+  $scope.setup = function() {
+    $http.get("http://localhost:3000/api/v1/beers").then(function(response) {
+      $scope.beers = response.data;
+      $scope.orderAttribute = 'name';
+      $scope.isOrderDescending = false;
+    });
+  };
+
+  $scope.addBeer = function(inputName, inputStyle, inputAlcohol) {
+    var newBeer = {
+      name: inputName,
+      style: inputStyle,
+      alcohol: inputAlcohol
+    };
+    $http.post("/api/v1/beers", newBeer).then(function(response) {
+      $scope.beers.push(response.data);
+    }, function(error) {
+      $scope.errors = error.data.errors;
+    });
+  };
+
+  $scope.updateBeer = function(inputBeer) {
+    var index = $scope.beers.indexOf(inputBeer);
+    if (index !== -1) {
+      var currentBeer = {
+      };
+      $http.patch("/api/v1/beers", currentBeer);
+    }
+  };
+
+  $scope.deleteBeer = function(inputBeer) {
+    $http.delete("/api/v1/beers/" + inputBeer.id).then(function(response) {
+      var index = $scope.beers.indexOf(inputBeer);
+      $scope.beers.splice(index, 1);
+    });
+  };
+
+  $scope.changeOrderAttribute = function(inputAttribute) {
+    if (inputAttribute === $scope.orderAttribute) {
+      $scope.isOrderDescending = !$scope.isOrderDescending;
+    } else {
+      $scope.isOrderDescending = false;
+    }
+    $scope.orderAttribute = inputAttribute;
+  };
+
+// For debugging in the browser
+  window.$scope = $scope;
+
+})();
--- app/controllers/api/v1/beers_controller.rb
+++ app/controllers/api/v1/beers_controller.rb
@@ -13,15 +13,13 @@ class Api::V1::BeersController < ApplicationController
     @beer = Beer.new(
       name: params[:name],
       style: params[:style],
-      hop: params[:hop],
-      yeast: params[:yeast],
-      malts: params[:malts],
-      ibu: params[:ibu],
-      alcohol: params[:alcohol],
-      blg: params[:blg]
+      alcohol: params[:alcohol]
       )
-    @beer.save
-    render "show.json.jbuilder"
+    if @beer.save
+      render "show.json.jbuilder"
+    else
+      render json: { errors: @beer.errors.full_messages }, status: 422
+    end
   end
 
   def update
@@ -29,14 +27,12 @@ class Api::V1::BeersController < ApplicationController
     @beer = Beer.find_by(id: beer_id)
     @beer.name = params[:name] || @beer.name
     @beer.style = params[:style] || @beer.style
-    @beer.hop = params[:hop] || @beer.hop
-    @beer.yeast = params[:yeast] || @beer.yeast
-    @beer.malts = params[:malts] || @beer.malts
-    @beer.ibu = params[:ibu] || @beer.ibu
     @beer.alcohol = params[:alcohol] || @beer.alcohol
-    @beer.blg = params[:blg] || @beer.blg
-    @beer.save
-    render "show.json.jbuilder"
+    if @beer.save
+      render "show.json.jbuilder"
+    else
+      render json: { errors: @beer.errors.full_messages}, status: 422
+    end
   end
 
   def destroy
